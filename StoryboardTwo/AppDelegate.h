5c2ae4f07b9d2be14e95a2c9070403288da8d90b
Soham Ghosh <sohamghosh@gmail.com>
2013-12-08 21:06:21 +0530

    Initial Commit

---
+++ StoryboardTwo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  StoryboardTwo
+//
+//  Created by Soham on 08/12/13.
+//  Copyright (c) 2013 TW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
